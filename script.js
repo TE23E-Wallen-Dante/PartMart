7982d3a40a0cad63882738960d1182e8312b1bba
Dante Wallén <dante.wallen@elev.ntig.se>
2025-02-14 09:17:13 +0100

    Lagt till produkt-rutor med flex wrap, samt knapp och container för utvalda produkter

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ function showDropdown() {
     var dropdown = document.getElementById("dropdown");
     dropdown.classList.toggle("active");
     console.log("Dropdown was clicked");
+
+    dropdown.style.transition = 'opacity 0.5s ease';
 }
