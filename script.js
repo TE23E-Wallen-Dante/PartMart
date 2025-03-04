848dd386fd815228139ae89ae101f4befa797923
Dante Wallén <dante.wallen@elev.ntig.se>
2025-03-04 15:03:07 +0100

    Bugfix - fixat så att menyn stängs (om den var synlig) ifall fönstrets storlek går från litet till stort.
    Har även uppdaterat kommentarer i javascriptet samt sorterat koden lite mer för att vara lättare att läsa och förstå.

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+// Funktion för att toggla menyn när man klickar på knappen
 function showDropdown() {
-    var dropdown = document.getElementById("dropdown");
-    dropdown.classList.toggle("active");
+
+    var dropdown = document.getElementById("dropdown"); // Skapar en variabel för alla element med id:t dropdown
+
+    dropdown.classList.toggle("active"); // Togglar klassen "active" på alla element med id:t dropdown
     console.log("Dropdown was clicked");
 
-    dropdown.style.transition = 'opacity 0.5s ease';
+    dropdown.style.transition = 'opacity 0.5s ease'; // Lite fina detaljer :)
 }
+
+// Stänger ner menyn ifall fönstrets storlek förändras
+// Kollar efter när fönstrets storlek förändras
+window.addEventListener("resize", () => {
+    const dropdown = document.getElementById("dropdown"); // Skapar en konstant för alla element med id:t dropdown
+    if (dropdown) dropdown.classList.remove("active"); // Om alla element med det id:t har klassen "active" tas den klassen bort -> detta för att dölja menyn ifall fönstrets storlek förändras
+});
